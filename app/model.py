36d20e1757a414f802a5b674b15b4ff7f00a212f
alireza-keivan <alirezakeyvan06@yahoo.com>
2026-07-23 16:05:21 +0800

    everything

---
+++ app/model.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+def model_creation():
+
+    model = YOLO('yolo12n.pt')
+    #results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+    results = model(source= 'tests/video1.mp4', vid_stride = 3, classes = [0], conf=0.1, show=True, save=True) # stream=True , visualize = True
+    
+    for result in results:  
+        xywh = result.boxes.xywh  # center-x, center-y, width, height
+        xywhn = result.boxes.xywhn  # normalized
+        xyxy = result.boxes.xyxy  # top-left-x, top-left-y, bottom-right-x, bottom-right-y
+        xyxyn = result.boxes.xyxyn  # normalized
+        names = [result.names[cls.item()] for cls in result.boxes.cls.int()]  # class name of each box
+        confs = result.boxes.conf  # confidence score of each box
+        print(names, confs)
+model_creation()
